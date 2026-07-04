e34fdf56c3af87d7e6daecddb5b6b80bca139a7d
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2026-07-04 13:25:46 +0200

    First implementation of rename.sh
    To be added/improved:
    Handle different filename types (for now, q3e-style demo names are hardcoded)
    Don't read players.json from disk with every iteration of the loop

--- rename.sh
+++ rename.sh
@@ -1,16 +1,30 @@
+#!/bin/bash
 ###########################################################################
 # Quake3_DemoHelpers - https://github.com/froschgrosch/Quake3_DemoHelpers #
 # Licensed under GNU GPLv3. - File: rename.sh                             #
 ###########################################################################
 
 
-file="demo.dm_68"
+#files=$(basename -a ./rename/input/*.dm_68)
 
-# get demo data
-udtoutput=$(zz_tools/UDT_json -a=g -c "./rename/input/$file")
+for file in ./rename/input/*.dm_68; do
+    file=$(basename -a $file)
+    echo Old: ${file/.dm_68/}
 
-# put player name into variable
-player=$(echo "$udtoutput" | jq -r .gameStates[0].demoTakerCleanName)
+    # get demo data
+    udtoutput=$(zz_tools/UDT_json -a=g -c "./rename/input/$file")
 
-# select true name
-player=$(cat zz_config/players.json | jq -r --arg name "$player" '( .[] | select(.names | index($name) != null) | .names[0] ) // ($name | sub("^LPG "; ""))')
+    player=$(echo "$udtoutput" | jq -r .gameStates[0].demoTakerCleanName)
+
+    # select canonical name
+    player=$(cat zz_config/players.json | jq -r --arg name "$player" '( .[] | select(.names | index($name) != null) | .names[0] ) // ($name | sub("^LPG "; ""))')
+
+    map=$(echo "$udtoutput" | jq -r .gameStates[0].configStringValues.mapname)
+
+    newname=$(echo ${file:0:4}-${file:4:2}-${file:6:2}_${file:8:2}-${file:10:2}-${file:12:2}_$map\_$player)
+
+    echo New: $newname ; echo
+
+    mv ./rename/input/$file ./rename/output/$newname.dm_68
+    touch -d "$(date -Rd "${file:0:4}-${file:4:2}-${file:6:2} ${file:8:2}:${file:10:2}:${file:12:2}")" ./rename/output/$newname.dm_68
+done
