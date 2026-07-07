ce7057d7a481efb9087fcdfeb229a2aca4d8f4fd
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2026-07-07 17:47:56 +0200

    First basic implementation of extract_highlight.sh
    Changed method of iterating through the chat messages because it did not work together with the selection of the action
    
    TBD:
    - Several different sanity checks
    - Clip name suffixes
    - Config file swapping
    - Adjusting start and end time of clip

--- extract_highlight.sh
+++ extract_highlight.sh
@@ -7,6 +7,8 @@
 for file in ./highlight/input/*.dm_68; do
     file=$(basename -a $file)
 
+    # todo: check if filename matches regex
+
     # get demo data
     udtoutput=$(zz_tools/UDT_json -a=mg -c "./highlight/input/$file")
 
@@ -24,9 +26,15 @@ for file in ./highlight/input/*.dm_68; do
 
     echo "Selecting $file..."
 
-    # iterate through chat messages
-    echo "$udtoutput" | jq -c '.chat.[]' | while read -r message; do
+    # preprocess values
+    gamename=$(echo "$udtoutput" | jq -r .gameStates[0].configStringValues.gamename)
+    demopath="$(jq -r '.q3install.path' ./zz_config/highlights/settings.json)/$gamename/demos/highlight_preview.dm_68"
+    execpath=$(jq -r '.q3install.path + "/" + .q3install.executable' ./zz_config/highlights/settings.json)
 
+    readarray -t messages < <(echo "$udtoutput" | jq -c '.chat.[]')
+
+    # iterate through chat messages
+    for message in "${messages[@]}"; do
         # check if the message is matching criteria
         if jq -e --argjson msg "$message" --arg demoTaker "$player" 'any(.[]; (.names | index($msg.cleanPlayerName) != null) and (.demoMarkers | index($msg.cleanMessage) != null) and (.names | index($demoTaker) != null))' ./zz_config/players.json > /dev/null;
         then
@@ -40,8 +48,60 @@ for file in ./highlight/input/*.dm_68; do
             zz_tools/UDT_cutter t -q -s="$starttime" -e="$endtime" -o="./highlight/temp" "./highlight/input/$file"
 
             clipfile=$(basename -a ./highlight/temp/*.dm_68)
-            mv ./highlight/temp/$clipfile ./highlight/output_clip/${clipfile/_CUT/}
-        fi
-    done
+
+            #echo ./highlight/temp/$clipfile "$demopath"
+            cp ./highlight/temp/$clipfile "$demopath"
+
+            # decision loop
+            while true; do
+                # play demo
+                $execpath +set fs_game "$gamename" +set fs_homepath "$(dirname "$execpath")" +set nextdemo 'quit' +demo 'highlight_preview.dm_68' &> /dev/null
+
+                # select action
+                echo 'c) Quit'
+
+                select action in 'Keep' 'Delete' 'Watch again'; do
+                    #echo "$REPLY $action"
+
+                    case $REPLY in
+                        # Quit - clean up and exit
+                        [Cc]*)
+                            rm $demopath
+                            exit 0
+                        ;;
+
+                        # Keep - move file to output folder
+                        1)
+                            # todo: add suffix to filename
+                            mv ./highlight/temp/$clipfile ./highlight/output_clip/${clipfile/_CUT/}
+
+                            break 2 # decision loop
+                        ;;
+
+                        # Keep - move file to output folder
+                        2)
+                            rm ./highlight/temp/$clipfile
+
+                            break 2 # decision loop
+                        ;;
+
+                        # Watch again
+                        3)
+                            break 1 # select statement
+                        ;;
+
+                        *)
+                        ;;
+                    esac
+                done # select loop
+            done # decision loop
+        fi # endif the message is matching criteria
+
+        # clean up remaining preview file
+        rm $demopath
+    done # message loop
+
+    # move demo to output folder
     mv ./highlight/input/$file ./highlight/output_demo/
-done
+done # file loop
+echo 'Demo processing is finished.'
